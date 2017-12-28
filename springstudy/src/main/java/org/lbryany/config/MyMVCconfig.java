a03148d240f3563bb019d581e4a394b624938cd4
Bryan Lee <bryan_ly@126.com>
2017-12-29 00:37:54 +0800

    create demo first
    Signed-off-by: Bryan Lee <bryan@BryandeMacBook-Pro.local>

--- springstudy/src/main/java/org/lbryany/config/MyMVCconfig.java
+++ springstudy/src/main/java/org/lbryany/config/MyMVCconfig.java
@@ -2,14 +2,23 @@ package org.lbryany.config;
 
 import org.apache.commons.dbcp2.BasicDataSource;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Collection;
+import java.util.Locale;
+
 @Configuration
 @EnableWebMvc
-
+@ComponentScan("org.lbryany")
 public class MyMVCconfig implements WebMvcConfigurer {
 
     /**
---
+++ springstudy/src/main/java/org/lbryany/controller/HelloController.java
@@ -0,0 +1,17 @@
+package org.lbryany.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+@Controller
+public class HelloController {
+
+    @ResponseBody
+    @RequestMapping(value = "/")
+    public String index(){
+        return "hello";
+    }
+}
