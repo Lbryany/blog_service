965e78fc30ecd6e229b7f8f889f4492fe8f924d6
Bryan Lee <bryan_ly@126.com>
2018-01-02 21:59:49 +0800

    生成初始化sql 初始化user表 创建user实体

---
+++ springdemo/src/main/java/org/bryanzzz/entity/User.java
@@ -0,0 +1,111 @@
+package org.bryanzzz.entity;
+
+import java.util.Date;
+
+public class User {
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userId=" + userId +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", access=" + access +
+                ", avatar='" + avatar + '\'' +
+                ", nickname='" + nickname + '\'' +
+                ", lastLoginTime=" + lastLoginTime +
+                ", createdAt=" + createdAt +
+                ", deleted=" + deleted +
+                '}';
+    }
+
+    private long userId;
+
+    private String username;
+
+    private String password;
+
+    private int access;
+
+    private String avatar;
+
+    private String nickname;
+
+    private Date lastLoginTime;
+
+    private Date createdAt;
+
+    private int deleted;
+
+    public long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(long userId) {
+        this.userId = userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getAccess() {
+        return access;
+    }
+
+    public void setAccess(int access) {
+        this.access = access;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public Date getLastLoginTime() {
+        return lastLoginTime;
+    }
+
+    public void setLastLoginTime(Date lastLoginTime) {
+        this.lastLoginTime = lastLoginTime;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public int getDeleted() {
+        return deleted;
+    }
+
+    public void setDeleted(int deleted) {
+        this.deleted = deleted;
+    }
+}
