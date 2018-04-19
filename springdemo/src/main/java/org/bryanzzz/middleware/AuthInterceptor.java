84b16617e23a302fcd28a54913c12b20a0f9e67b
Bryan Lee <bryan_ly@126.com>
2018-04-19 22:13:41 +0800

    添加拦截器jwt验证判断

--- springdemo/src/main/java/org/bryanzzz/middleware/AuthInterceptor.java
+++ springdemo/src/main/java/org/bryanzzz/middleware/AuthInterceptor.java
@@ -1,5 +1,8 @@
 package org.bryanzzz.middleware;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.MalformedJwtException;
+import org.bryanzzz.common.JsonWebToken;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 import javax.servlet.http.HttpServletRequest;
@@ -13,8 +16,29 @@ public class AuthInterceptor extends HandlerInterceptorAdapter{
         //判断请求的是否是需要权限的页面，比如 发布 ，后台管理 ，登出 页面 （已经在拦截器配置xml中做了）
             String token = request.getParameter("token");
 
+            //如果token没有传
             if(token == null){
+                //判断有没有session
+                response.sendError(401, "权限认证失败");
                 return false;
+            }else{
+                //TODO 先写死一个管理员用户
+                try {
+                    long nowMillis = System.currentTimeMillis();
+                    Claims userClaims = JsonWebToken.parseJWT(token);
+                    if(userClaims.getExpiration().getTime() < nowMillis){
+                        response.sendError(401, "token已过期");
+                        return false;
+                    }else if (Integer.parseInt(userClaims.getId()) != 1000){
+                        //写死可登陆的用户id
+                        response.sendError(401, "登录用户不正确");
+                        return false;
+                    }
+                }catch (MalformedJwtException e){
+                    response.sendError(401, "token解码失败");
+                    return false;
+                }
+
             }
 
         return true;
